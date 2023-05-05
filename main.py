69842e507386fe8412767fb3c0ac116ae4ba43cc
rockbjson <91388777+rockbjson@users.noreply.github.com>
2023-05-05 06:41:14 -0400

    update snake movement in main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import pygame
+import snake
+
 
 pygame.init()
 
@@ -43,6 +45,25 @@ start_text, start_rect = render_text(start_font, "Press any key to start", dark_
 running = True
 start_game = False
 
+x = 250
+y = 250
+new_x = 1
+new_y = 0
+
+clock = pygame.time.Clock()
+
+# display snake
+def snake(image):
+    global x, y
+
+    x = x + new_x
+    y = y + new_y
+
+    head_image = pygame.image.load(image).convert_alpha()
+
+    screen.blit(background, (0, 0))
+    screen.blit(head_image, (x%width, y%height))
+
 # game loop
 while running:
     for event in pygame.event.get():
@@ -51,15 +72,50 @@ while running:
         elif event.type == pygame.KEYDOWN:
             start_game = True
 
-    if start_game:
-        # start the game here
-        pass
-
     screen.blit(background, (0, 0))
     screen.blit(snake_image, snake_rect)
     screen.blit(title_text_bg, title_bg_rect)
     screen.blit(title_text, title_rect)
     screen.blit(start_text, start_rect)
+
+    if start_game:
+        # start the game here
+        screen.blit(background, (0,0))
+
+        # change snake head direction
+        if (event.type == pygame.KEYDOWN):
+            head_direction = "head_right.png"
+            if (event.key == pygame.K_LEFT):
+                if (new_x != 1):
+                    new_x = -1
+                    head_direction = "head_left.png"
+                new_y = 0
+            elif (event.key == pygame.K_RIGHT):
+                if (new_x != -1):
+                    new_x = 1
+                    head_direction = "head_right.png"
+                new_y = 0
+            elif (event.key == pygame.K_UP):
+                if (new_y != 1):
+                    new_y = -1
+                    head_direction = "head_up.png"
+                new_x = 0
+            elif (event.key == pygame.K_DOWN):
+                if (new_y != -1):
+                    new_y = 1
+                    head_direction = "head_down.png"
+                new_x = 0
+            else:
+                continue
+            snake(head_direction)
+
+        # continue moving when there is no key down event
+        if (not pygame.event.get()):
+            snake(head_direction)
+
+    # set game speed
+    clock.tick(100)
+
     pygame.display.flip()
 
 pygame.quit()
