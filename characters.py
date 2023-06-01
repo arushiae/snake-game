fb5868a65a7f4db046b5cf7d6ae1ccf44208d4bc
arushi <127650652+arushiae@users.noreply.github.com>
2023-06-02 00:04:45 +0530

    Update characters.py

--- characters.py
+++ characters.py
@@ -1,6 +1,11 @@
-from pygame import Vector2
-from init import *
 import random
+
+from init import *
+
+from pygame import Vector2
+
+# sound effects
+point_gained_sound = mixer.Sound('music/gameboy-pluck.mp3')
 
 class Characters():
     def __init__(self):
@@ -8,14 +13,17 @@ class Characters():
         self.mouse_x = random.randint(0, cell_number - 1)
         self.mouse_y = random.randint(0, cell_number - 1)
         self.pos = Vector2(self.mouse_x, self.mouse_y)  # storing x and y position in a 2D vector
-        self.image = pygame.image.load('mouse.png').convert_alpha()
+        self.image = pygame.image.load('graphics/mouse.png').convert_alpha()
         self.image_resized = pygame.transform.scale(self.image, (cell_size, cell_size))
 
         # initialize snake attributes
         self.snake_x, self.snake_y = 250, 250
         self.new_x, self.new_y = 25, 0
-        self.head_direction = "head_left.png"
+        self.head_direction = "graphics/head_right.png"
         self.snake_body = [(self.snake_x, self.snake_y)]
+
+        # points system
+        self.points = 0
 
 # snake movement
     def display_character(self, image):
@@ -40,44 +48,47 @@ class Characters():
             if (event.key == pygame.K_LEFT):
                 if (self.new_x != 25):
                     self.new_x = -25
-                    self.head_direction = "head_left.png"
+                    self.head_direction = "graphics/head_left.png"
                 self.new_y = 0
             elif (event.key == pygame.K_RIGHT):
                 if (self.new_x != -25):
                     self.new_x = 25
-                    self.head_direction = "head_right.png"
+                    self.head_direction = "graphics/head_right.png"
                 self.new_y = 0
             elif (event.key == pygame.K_UP):
                 if (self.new_y != 25):
                     self.new_y = -25
-                    self.head_direction = "head_up.png"
+                    self.head_direction = "graphics/head_up.png"
                 self.new_x = 0
             elif (event.key == pygame.K_DOWN):
                 if (self.new_y != -25):
                     self.new_y = 25
-                    self.head_direction = "head_down.png"
+                    self.head_direction = "graphics/head_down.png"
                 self.new_x = 0
 
         return self.head_direction
 
     # checks for snake and mouse collision and updates mouse coordinates if true
     def check_mouse_collision(self):
-        if (self.mouse_x == int(self.snake_x/cell_size) and self.mouse_y == int(self.snake_y/cell_size)):
-            while ((self.mouse_x*cell_size, self.mouse_y*cell_size) in self.snake_body):
+        if self.mouse_x == int(self.snake_x/cell_size) and self.mouse_y == int(self.snake_y/cell_size):
+            while (self.mouse_x * cell_size, self.mouse_y * cell_size) in self.snake_body:
                 self.update_mouse_location()
+                # update points and play sound
+                self.points += 1
+                point_gained_sound.play()
         else:
             del self.snake_body[0]
 
     def draw_snake(self, image, head_image):
         for (i,j) in self.snake_body:
-            if (self.snake_body[len(self.snake_body)-1]== (i, j)):
+            if self.snake_body[len(self.snake_body)-1] == (i, j):
                 screen.blit(head_image, (i, j))
             else:
-                if (image == 'head_right.png' or image == 'head_left.png'):
-                    body_image = pygame.image.load('body_hor.png').convert_alpha()
+                if (image == 'graphics/head_right.png' or image == 'graphics/head_left.png'):
+                    body_image = pygame.image.load("graphics/body_hor.png").convert_alpha()
                     screen.blit(body_image, (i, j))
                 else:
-                    body_image = pygame.image.load('body_vert.png').convert_alpha()
+                    body_image = pygame.image.load("graphics/body_vert.png").convert_alpha()
                     screen.blit(body_image, (i, j))
 
     def draw_mouse(self):
@@ -85,11 +96,12 @@ class Characters():
         screen.blit(self.image_resized, mouse_rect)
 
     def get_mouse_location(self):
-        return (self.mouse_x, self.mouse_y)
+        return self.mouse_x, self.mouse_y
 
     def update_mouse_location(self):
         self.mouse_x = random.randint(0, cell_number - 1)
         self.mouse_y = random.randint(0, cell_number - 1)
         self.pos = Vector2(self.mouse_x, self.mouse_y)  # storing x and y position in a 2D vector
-        self.image = pygame.image.load('mouse.png').convert_alpha()
+        self.image = pygame.image.load('graphics/mouse.png').convert_alpha()
         self.image_resized = pygame.transform.scale(self.image, (cell_size, cell_size))
+        
